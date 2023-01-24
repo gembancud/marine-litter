b7dfe7e14e3975dd29da294c16a6e51e2a501a59
aahlexxx <aclabanon@up.edu.ph>
2023-01-24 14:20:30 +0800

    edit function

--- gps/main.py
+++ gps/main.py
@@ -39,13 +39,15 @@ def gps(value_lon, value_lat):
         print('Already positioned')
     else:
         print('No positioning')
-    print('Time %d:'%x.Time_H,end='')
-    print('%d:'%x.Time_M,end='')
-    print('%d'%x.Time_S)
-    print('Lon = %f'%x.Lon,end='')
-    print(' Lat = %f'%x.Lat)
+    #print('Time %d:'%x.Time_H,end='')
+    #print('%d:'%x.Time_M,end='')
+    #print('%d'%x.Time_S)
+    #print('Lon = %f'%x.Lon,end='')
+    #print(' Lat = %f'%x.Lat)
     value_lon = x.Lon
     value_lat = x.Lat
+    print('Lattitude: %f'%value_lat)
+    print('Longitude: %f'%value_lon)
     #x.L76X_Baidu_Coordinates(x.Lat, x.Lon)
     #print('Baidu coordinate %f'%x.Lat_Baidu,end='')
     #print(',%f'%x.Lon_Baidu)
