d8e974ac6d7f034f074932b38c4703068daaffca
Gil Emmanuel Bancud <gembancud@gmail.com>
2023-01-24 16:54:17 +0800

    Config fixes for yolov5

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ WINDOW_TITLE = "CSI Camera"
 WIDTH = 640
 HEIGHT = 480
 # Model configuration
-YOLOV5_PLUGIN_LIBRARY = "libmyplugins.so"
-YOLOV5_ENGINE_FILE_PATH = "aceluya_n.engine"
+YOLOV5_PLUGIN_LIBRARY = "models/yolov5/libmyplugins.so"
+YOLOV5_ENGINE_FILE_PATH = "models/yolov5/aceluya_n.engine"
 
 CONF_THRESH = 0.5
 IOU_THRESHOLD = 0.4
